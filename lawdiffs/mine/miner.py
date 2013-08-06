1959cd6df664e262821f5fe8a064fc33fd8d6148
Harvey Rogers <harveyr@gmail.com>
2013-08-06 07:48:38 -0700

    More work on pdf parsing.

--- lawdiffs/mine/miner.py
+++ lawdiffs/mine/miner.py
@@ -157,9 +157,9 @@ class LawParser(object):
                 except AttributeError:
                     pass
 
-            if i > 5:
-                logger.warn('breaking on page {}'.format(i))
-                break
+            # if i > 10:
+            #     logger.warn('breaking on page {}'.format(i))
+            #     break
 
         text_buffer = re.sub(r'\s{2}', ' ', text_buffer)
         self.cache_pdf_text(url, text_buffer)
@@ -171,10 +171,10 @@ class LawParser(object):
 
     def fetch_pdf_text(self, url, callback):
         """Fetch pdf text from url"""
-        # cached = self.fetch_cached_pdf_text(url)
-        # if cached:
-        #     callback(cached)
-        #     return
+        cached = self.fetch_cached_pdf_text(url)
+        if cached:
+            callback(cached)
+            return
         self.with_open_pdf(url, self.extract_pdf_text, callback)
 
     def get_soup_text(self, soup_elem):
@@ -205,7 +205,8 @@ class OrLawParser(LawParser):
     volume_pat_html = re.compile(r'ORS Volume (\d+),')
     chapter_pat_html = re.compile(r'ORS Chapter (\d+)')
     pdf_subsection_re = re.compile(r'\b(\d+\.\d+)\b')
-    pdf_footer_re = re.compile(r'Title \d+ Page \d+ \(\d+ Edition\) [A-Z\s;]+ \d+\.\d+\s')
+    pdf_footer_re = re.compile(
+        r'Title \d+ Page \d+ \(\d+ Edition\) \d+\.\d+ [A-Z;\s]+')
 
     sources = [
         {
@@ -260,7 +261,7 @@ class OrLawParser(LawParser):
     def pdf_text_pre_append_hook(self, text):
         # logger.debug('text: {v}'.format(v=text))
         # if text.isupper():
-        #     return ''
+        #     logger.debug('upper: ' + text)
         # if re.match(r'(Title|Page) \d+', text):
         #     return ''
         # if re.match(r'\(\d+ Edition\)', text):
@@ -278,6 +279,8 @@ class OrLawParser(LawParser):
             break
 
     def create_law_from_pdf_text(self, text, subsection, next_subsection=None):
+        logger.debug('subsection: {v}'.format(v=subsection))
+        logger.debug('next_subsection: {v}'.format(v=next_subsection))
         if not re.match(r'{}'.format(subsection), text):
             raise Exception(
                 'Text does not start with subsection {}'.format(
@@ -289,28 +292,33 @@ class OrLawParser(LawParser):
         law = data_laws.get_or_create_statute(subsection, self.law_code)
         text = text[len(subsection):].lstrip()
 
-        parts = re.split(r'({})'.format(next_subsection), text)
+        next_section_split = next_subsection.split('.')
+        next_section_pat = r'([\.:\]] {}\.{} [A-Z][a-z])'.format(
+            next_section_split[0], next_section_split[1])
+        next_section_hit = re.search(next_section_pat, text)
+        if not next_section_hit:
+            raise Exception(
+                'Could not find next section {} with text: "{}..."'.format(
+                    next_subsection, text[:3000]))
 
-        first_part = parts[0].strip()
-        # logger.debug('first_part: {v}'.format(v=first_part))
-        if first_part.startswith('['):
-            if not first_part.endswith(']'):
+        this_section_end_index = next_section_hit.start() + 2
+
+        subsection_text = text[:this_section_end_index].strip()
+        # logger.debug('subsection_text: {v}'.format(v=subsection_text))
+        if subsection_text.startswith('['):
+            if not subsection_text.endswith(']'):
                 raise Exception(
-                    'Unmatched brackets: {}'.format(first_part))
-            law.set_version_text(self.current_version, first_part)
+                    'Unmatched brackets: {}'.format(subsection_text))
+            law.set_version_text(self.current_version, subsection_text)
             return ' '.join(parts[1:])
 
-        title_split = first_part.split('.', 1)
+        title_split = subsection_text.split('.', 1)
         law.set_version_title(self.current_version, title_split[0])
-
-        text_buffer = title_split[1]
-        return_parts_index = 1
-        while not text_buffer.rstrip().endswith(']'):
-            print('here!')
-            return_parts_index += 1
-            break
-        law.set_version_text(self.current_version, text_buffer)
-        return (law, ' '.join(parts[return_parts_index:]))
+        law.set_version_text(self.current_version, title_split[1])
+        remainder = text[this_section_end_index:]
+        # logger.debug('returning remainder: {v}...'.format(
+            # v=remainder[:200]))
+        return (law, remainder)
         # next_section_hit = re.search()
         # logger.debug('next_section_hit: {v}'.format(v=next_section_hit))
 
@@ -348,14 +356,24 @@ class OrLawParser(LawParser):
         first_subsection_re = re.compile(r'{}'.format(first_subsection))
         second_hit = first_subsection_re.search(text, first_hit.end())
         text = text[first_hit.start():second_hit.start()]
-        logger.debug('text: {v}'.format(v=text))
         subsections = self.pdf_subsection_re.findall(text)
-        return (sorted(subsections, key=float), second_hit.start())
+        unique = []
+        for s in subsections:
+            if s not in unique:
+                unique.append(s)
+        return (sorted(unique, key=float), second_hit.start())
 
     def create_laws_from_pdf_text(self, text):
         """Assume one, big, pre-extracted string."""
         # Title \d+ Page \d+ \(\d{4} Edition\) [A-Z\s;]+
-        text = self.pdf_footer_re.sub(' ', text)
+        text = self.pdf_footer_re.sub('', text)
+
+        heading_matches = re.findall(r'[A-Z]+[A-Z\s]+\d+\.\d+', text)
+        for match in heading_matches:
+            if match.startswith('ORS'):
+                continue
+            sub_str = [s for s in re.split(r'(\d+\.\d+)', match) if s][1]
+            text = text.replace(match, sub_str)
 
         first_subsection = None
         first_subsection_reached = False
@@ -368,17 +386,13 @@ class OrLawParser(LawParser):
             text)
         text = text[start_index:]
         current_law = None
-        processed_subsections = []
-        logger.debug('subsections: {v}'.format(v=subsections))
         for i in range(len(subsections) - 1):
             subsection = subsections[i]
-            if subsection in processed_subsections:
-                continue
-            logger.debug('subsection: {v}'.format(v=subsection))
+            logger.debug('starting subsection: {v}'.format(v=subsection))
             if not re.match(r'{}'.format(subsection), text):
                 exception_print_idx = min(600, len(text))
                 raise Exception(
-                    'Text does not match expected subsection {}: {}'.format(
+                    'Text does not match expected subsection {}: "{}"'.format(
                         subsection, text[:exception_print_idx]))
             if i == len(subsections) - 1:
                 next_subsection = None
